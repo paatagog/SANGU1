f97cc4daf9fe7b5b87de8ed89f979dc9290a7107
NickKvizhinadze <nickkvizhinadze@gmail.com>
2013-10-08 13:46:36 +0400

    Add Prime Number Counter Programm

---
+++ kvizhinadze/PrimeNumbers.java
@@ -0,0 +1,27 @@
+package kvizhinadze;
+
+public class PrimeNumbers {
+	public static void main(String[] args){
+		double number = 3;
+		int divisor;
+		
+		System.out.print("1, 2, ");
+		
+		while(number <= 1000){
+			boolean is_prime = true;
+			for(divisor = 2; divisor < Math.sqrt(number); divisor++){
+				if (number % divisor == 0){
+					is_prime = false;
+					break;
+				}
+								
+			}
+			if (is_prime){
+				int int_number = (int)number;
+				System.out.print(int_number + ", ");
+			}
+			
+			number++;
+		}
+	}
+}
