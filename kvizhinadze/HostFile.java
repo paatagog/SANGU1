997aed302032b5a6ee5af6a6f3a001d36e1aa7ee
NickKvizhinadze <nickkvizhinadze@gmail.com>
2013-11-09 11:15:38 +0400

    Edit IpAddress and Hostfile classes

--- kvizhinadze/HostFile.java
+++ kvizhinadze/HostFile.java
@@ -11,9 +11,15 @@ public static String INPUT_FILES = "C:/hosts";
 	
 	public static void main(String[] args){
 		try{
+			IpAddressChecker checker = new IpAddressChecker();
 			List<String> lines = Files.readAllLines(Paths.get(INPUT_FILES), Charset.forName("UTF-8"));
 			for(String line:lines){
-				System.out.println(line);
+				if (line != null && !line.trim().startsWith("#")) {
+					String[] arr = line.split(" ");
+					if (!checker.isMatch(arr[1], arr[0])) {
+						System.out.println("Error: " + arr[0] + " " + arr[1]);
+					}
+				}
 			}
 		} catch (IOException e){
 			e.printStackTrace();
